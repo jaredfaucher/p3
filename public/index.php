3a9a350452a4f21c9d42cebb18a67ec68504701d
Jared Faucher <jared.faucher@gmail.com>
2014-07-09 18:10:21 -0400

    changed index.php for test

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
-
+//small change
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
